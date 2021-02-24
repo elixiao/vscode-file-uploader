c4be57f01b3d2e740334424d9dcb871866c04d96
keliq <webassembly@qq.com>
2021-02-24 16:31:25 +0800

    fix: codeLens bug

--- src/codelens/CustomCodeLensProvider.ts
+++ src/codelens/CustomCodeLensProvider.ts
@@ -24,30 +24,33 @@ export class CustomCodeLensProvider implements CodeLensProvider {
   public provideCodeLenses(document: TextDocument): ProviderResult<CodeLens[]> {
     const content: string = document.getText();
     if (content.indexOf("var FileUploader") < 0) return [];
-    if (![".js", ".ts"].includes(path.extname(document.fileName))) return [];
+    if (![ ".js", ".ts" ].includes(path.extname(document.fileName))) return [];
     const codeLens: CodeLens[] = [];
     const lineCount = document.lineCount;
     let codeLensLineStart: number = 0;
     let codeLensLineEnd: number = 0;
     let range: Range = new Range(0, 0, 0, 0);
     let lines: string[] = [];
+    let open = false;
     for (let i: number = 0; i < lineCount; i++) {
       const lineContent: string = document.lineAt(i).text;
       lines.push(lineContent);
-      if (lineContent.startsWith("var FileUploader ")) {
-        lines = [lineContent];
+      if (!open && /(var|let|const)\s+FileUploader/.test(lineContent)) {
+        lines = [ lineContent ];
         codeLensLineStart = i;
         range = new Range(codeLensLineStart, 0, codeLensLineStart, 0);
-      } else if (lineContent.startsWith("}")) {
+        open = true;
+      } else if (open && lineContent.startsWith("}")) {
         codeLensLineEnd = i;
         codeLens.push(
           new CodeLens(range, {
             title: `▶ ${t("codeLens.upload")}`,
             command: "file-uploader.uploadByCodeLens",
-            arguments: [lines],
+            arguments: [ lines ],
           })
         );
         lines = [];
+        open = false;
       }
     }
     return codeLens;
